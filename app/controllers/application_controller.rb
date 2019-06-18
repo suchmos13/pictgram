dc1dd436fc839bee57b46499dcac14592b3484cf
kento <kurokoganekoku13@gmail.com>
2019-06-18 07:43:45 +0000

    rails 05 課題

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   
   add_flash_types :success, :info, :warning, :danger
   
+  helper_method :current_user, :logged_in?
+  
+  
+  private 
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+  !current_user.nil?
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
   get 'users/new'
+  
   root 'pages#index'
   get 'pages/help'
   
   resources :users
+  resources :topics
+  
+  get   '/login',   to: 'sessions#new'
+  post  '/login',   to: 'sessions#create'
+  delete'/logout',   to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
